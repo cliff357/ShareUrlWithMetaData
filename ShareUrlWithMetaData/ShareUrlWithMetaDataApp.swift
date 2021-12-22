afb947b393369dfca040fc33d07c0d0ec91c16d9
Cliff Chan <cliff.chan@foodmarco.com>
2021-12-22 11:08:30 +0800

    Initial Commit

---
+++ ShareUrlWithMetaData/ShareUrlWithMetaDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShareUrlWithMetaDataApp.swift
+//  ShareUrlWithMetaData
+//
+//  Created by Cliff Chan on 22/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct ShareUrlWithMetaDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
